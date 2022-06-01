e6c4343ec9208e25715c773c4bd7a2f86b178860
sj_kim <ksj961223@gmail.com>
2022-06-02 02:45:19 +0900

    deploy

--- src/db/index.js
+++ src/db/index.js
@@ -12,7 +12,7 @@ import { ImageModel } from "./models/Image";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URL = process.env.MONGO;
+const MONGO_URL = process.env.MONGODB_URL;
 mongoose
   .connect(MONGO_URL)
   .then(() => console.log(`${MONGO_URL}에 연결 성공!`))
