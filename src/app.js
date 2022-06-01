a4a4724567b866676e1daf459a4ec75689672161
sj_kim <ksj961223@gmail.com>
2022-06-02 02:43:54 +0900

    deploy

--- src/app.js
+++ src/app.js
@@ -24,10 +24,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("안녕하세요, 레이서 프로젝트 API 입니다.");
-});
-
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -96,5 +92,11 @@ app.use(BoardRouter);
 app.use(ImageRouter);
 // errorMessage yellow
 app.use(errorMiddleware);
+
+import path from "path";
+app.use(express.static(path.join(__dirname, "../front", "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front", "build", "index.html"));
+});
 
 export { app };
